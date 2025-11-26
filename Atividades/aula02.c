38d8889f9ea2b436bbedad77876a631c02a15af5
Samuel Ismael <samuel.ismael@academico.ufpb.br>
2025-11-26 16:03:06 -0300

    feat: add bubbleSort in aula02.

--- Atividades/aula02.c
+++ Atividades/aula02.c
@@ -2,16 +2,35 @@
 #include <stdio.h>
 
 int main(){
+    
     int v[5] = {7,1,9,6,5};
-    int vCopia[5];
-
-    for (int i = 0; i < sizeof(v); i++){
-        vCopia[i] = v[i];
+    int vTamanho = sizeof(v) / sizeof(v[0]); // Calcula o tamanho do vetor;
+    int vCopia[5];                           // sizeOf(v) (5 ints → 5 × 4 bytes = 20 bytes)
+                                             // divide (/) por sizeOf(v[0]) 4 bytes: 20 / 4 = 5
+    // Faz uma cópia do vetor original
+    for (int a = 0; a < vTamanho; a++){
+        vCopia[a] = v[a];
     }
 
-    printf("%d", vCopia);
-    
+    // Print de teste para ver se o vetor foi copiado corretamente
+    for (int b = 0; b < vTamanho; b++){
+        printf("%d - ", vCopia[b]);
+    }
 
-    //Aplicar algoritmo de ordenação, quero o InsertSorte
-    
+    // Bubble sort
+    int temp;
+    for (int i = 0; i < vTamanho - 1; i++){
+        for (int j = 0; j < vTamanho -1 -i; j++){
+            if (vCopia[j] > vCopia[j+1]){
+                temp = vCopia[j];
+                vCopia[j] = vCopia[j+1];
+                vCopia[j+1] = temp;
+            } 
+        }
+    }
+
+    // Print para ver se o vetor foi organizado corretamente 
+    for (int c = 0; c < vTamanho; c++){
+        printf("%d - ", vCopia[c]);
+    }    
 }
